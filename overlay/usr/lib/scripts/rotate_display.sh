98e5a588d760f20dbeefb46cb5b71d56274070b1
mingtsai <mingtsai@ieiworld.com>
2024-06-21 17:13:12 +0800

    overlay: scripts: rotate_display.sh: grep correct value of env
    Modify USER to USERNAME to avoid get same value twice.

--- overlay/usr/lib/scripts/rotate_display.sh
+++ overlay/usr/lib/scripts/rotate_display.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-name=$(env | grep USER | cut -d '=' -f2-);
+name=$(env | grep USERNAME | cut -d '=' -f2-);
 path=/home/$name/.config/monitors.xml
 if ! [ -f $path ];then
 	sleep 0.1 && xrandr --output DSI-2 --off --output HDMI-2 --left-of DSI-1 --output HDMI-1 --left-of HDMI-2 --output DSI-1 --rotate right
