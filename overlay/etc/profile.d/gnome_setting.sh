e1d2a681ed06b6589fcffe8d22fed7c1e26a2488
mingtsai <mingtsai@ieiworld.com>
2024-06-21 17:09:02 +0800

    overlay: profile.d: gnome_setting.sh: set power key suspend

--- overlay/etc/profile.d/gnome_setting.sh
+++ overlay/etc/profile.d/gnome_setting.sh
@@ -1,2 +1,3 @@
 gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
 gsettings set org.gnome.desktop.session idle-delay 0
+gsettings set org.gnome.settings-daemon.plugins.power power-button-action 'suspend'
