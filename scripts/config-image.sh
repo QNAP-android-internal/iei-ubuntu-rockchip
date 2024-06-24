2905d422884f02afe8fd46cbe99556f1a928a2d3
mingtsai <mingtsai@ieiworld.com>
2024-06-24 18:40:52 +0800

    scripts: config-image.sh: add module_alsa_source.pa to rootfs for load PulseAudio module

--- scripts/config-image.sh
+++ scripts/config-image.sh
@@ -280,6 +280,10 @@ for type in $target; do
     # Install packages for peripheral devices test
     chroot ${chroot_dir} /bin/bash -c "apt-get -y install iperf stress-ng memtester glmark2 jq minicom gnome-sound-recorder"
 
+    # intsall module_alsa_source.pa for load PulseAudio module
+    mkdir -p ${chroot_dir}/etc/pulse/default.pa.d/
+    cp -rv ${overlay_dir}/etc/pulse/default.pa.d/module_alsa_source.pa ${chroot_dir}/etc/pulse/default.pa.d/module_alsa_source.pa
+
     if [[ ${type} == "desktop" ]]; then
         if [ "${BOARD}" == orangepi-5 ] || [ "${BOARD}" == orangepi-5b ] || [ "${BOARD}" == nanopi-r6c ] || [ "${BOARD}" == nanopi-r6s ] || [ "${BOARD}" == turing-rk1 ]; then
             echo "set-default-sink alsa_output.platform-hdmi0-sound.stereo-fallback" >> ${chroot_dir}/etc/pulse/default.pa
