e457117dccb0fcf25a6aa8ccadf16054ea781967
mingtsai <mingtsai@ieiworld.com>
2024-06-24 14:16:52 +0800

    overlay: add extensions for control multi disply backlight
    Because ubuntu original screen brightness slider can change only one
    display's backlight,so add soft-brightness@fifi.org to make
    brightness slider can switch to different display and change its
    backlight.
    
    1.gnome_setting.sh: enable soft-brightness@fifi.org extensions
    2.soft-brightness@fifi.org: the extensions can switch different display
      and control backlight by screen brightness slider.
    
    Reference sources from https://github.com/F-i-f/soft-brightness
    LICENSE base on GPL-V3 as follows link:
    https://github.com/F-i-f/soft-brightness/blob/master/LICENSE

--- overlay/etc/profile.d/gnome_setting.sh
+++ overlay/etc/profile.d/gnome_setting.sh
@@ -1,3 +1,4 @@
 gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
 gsettings set org.gnome.desktop.session idle-delay 0
 gsettings set org.gnome.settings-daemon.plugins.power power-button-action 'suspend'
+gsettings set org.gnome.shell enabled-extensions "['soft-brightness@fifi.org']"
