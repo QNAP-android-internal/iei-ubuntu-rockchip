f415ebcd495ac8d34864eb614129a017c5eb045b
mingtsai <mingtsai@ieiworld.com>
2024-06-28 10:28:03 +0800

    jenkins.sh: add release note and md5 for check formal release

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+make_releasenote()
+{
+        release_name=$(git tag | grep UBUNTU | tail -1)
+        author="Ming Tsai"
+        os="UBUNTU_2204"
+        cp ${release_path}/ReleaseNotes.txt ${release_path}/release_template/ReleaseNotes_${os}.txt
+        sed -i "s/^[ \t]*Version:.*$/Version: ${release_name}/" ${release_path}/release_template/ReleaseNotes_${os}.txt
+        sed -i -r "s/^[ \t]*Date: [0-9]*\.[0-9]*\.[0-9]*(.*)/Date: $(date '+%Y.%m.%d') \1/" ${release_path}/release_template/ReleaseNotes_${os}.txt
+        sed -i "s/Howard Chou/${author}/g" ${release_path}/release_template/ReleaseNotes_${os}.txt
+}
+
 B675_path="/home/iei-sw/new_ssd1_4TB/rk3588/ubuntu_b675_rk3588"
 release_path="/home/iei-sw/new_ssd1_4TB/rk3588/release/b675/ubuntu2204"
-release_name="B675-Ubuntu-2204-R001.zip"
-
+release_name="B675-UBUNTU-2204-R001.zip"
 cd ${B675_path}
 rm -rf images
 rm -f build/linux-*.deb
@@ -14,10 +24,11 @@ if [ "$VARIANT" = "SERVER" ]; then
 else
     ./build.sh -b "$BOARD" -do
 fi
-
 if [ -f $release_path/$release_name ]; then
     rm -f $release_path/$release_name
 fi
+
+make_releasenote
 
 cd ${B675_path}/images
 for i in image-release-rockchip-format*.img.xz; do [ -f "$i" ] && built_file="$i"; done
@@ -28,7 +39,14 @@ fi
 
 release_folder=$(basename ${release_name} .zip)
 cp -a ${release_path}/release_template ${release_path}/${release_folder}
+rm ${release_path}/release_template/ReleaseNotes_${os}.txt
+
 built_file_zip=$(basename ${built_file} .xz).zip
-xz -c -d ${built_file} | zip ${release_path}/${release_folder}/${built_file_zip} -
-zip -r ${release_path}/${release_name} ${release_path}/${release_folder}
-rm -rf ${release_path}/${release_folder}
+release_img=$(basename ${built_file} .xz)
+md5_img=$(basename ${release_img} .img)
+xz -d ${built_file}
+md5sum ${release_img} > ${release_path}/${release_folder}/${md5_img}_md5sum.txt
+mv ${release_img} ${release_path}/${release_folder}/
+cd ${release_path}
+zip -r ${release_name} ${release_folder}
+rm -rf ${release_folder}
