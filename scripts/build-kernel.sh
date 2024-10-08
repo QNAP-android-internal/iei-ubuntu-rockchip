8841c146846c9cad39400d9058b2a7e5f857025f
lc-wang <lcwang@Ieiworld.com>
2024-10-08 15:38:40 +0800

    scripts: build-kernel: add the necessary file
    The necessary file include kernel configuration and Debian
    directories, which are used for creating Debian packages

--- scripts/build-kernel.sh
+++ scripts/build-kernel.sh
@@ -8,6 +8,8 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
+TOPDIR=$(pwd)
+
 cd "$(dirname -- "$(readlink -f -- "$0")")" && cd ..
 mkdir -p build && cd build
 
@@ -32,6 +34,26 @@ export $(dpkg-architecture -aarm64)
 export CROSS_COMPILE=aarch64-linux-gnu-
 export CC=aarch64-linux-gnu-gcc
 export LANG=C
+
+if [[ "${SUITE}" == iei-b675-noble  ]]; then
+export $(dpkg-architecture -aarm64)
+export CROSS_COMPILE=${TOPDIR}/tools/prebuilts/gcc/linux-x86/aarch64/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
+export CC=${TOPDIR}/tools/prebuilts/gcc/linux-x86/aarch64/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-gcc
+export LANG=C
+
+make ARCH=arm64 \
+	CROSS_COMPILE=${TOPDIR}/tools/prebuilts/gcc/linux-x86/aarch64/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu- \
+	iei_android_defconfig
+
+mv .config ../.config
+
+make ARCH=arm64 \
+    CROSS_COMPILE=${TOPDIR}/tools/prebuilts/gcc/linux-x86/aarch64/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu- \
+    mrproper
+
+    cp -r ../../packages/kernel-${SUITE}/debian .
+    cp -r ../../packages/kernel-${SUITE}/debian.rockchip .
+fi
 
 # Compile the kernel into a deb package
 fakeroot debian/rules clean binary-headers binary-rockchip do_mainline_build=true
