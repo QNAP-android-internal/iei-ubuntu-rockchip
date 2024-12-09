6cadbf8904270336f9b74cda11bcf456482ee822
lcwang <lcwang@ieiworld.com>
2024-12-09 16:02:38 +0800

    config: suites: update the kernel version

--- config/suites/iei-b675-noble.sh
+++ config/suites/iei-b675-noble.sh
@@ -4,7 +4,7 @@ export RELASE_NAME="Ubuntu 24.04 LTS (Noble Nombat)"
 export RELASE_VERSION="24.04"
 
 export KERNEL_REPO="https://github.com/QNAP-android-internal/kernel-rk.git"
-export KERNEL_BRANCH="iei-android-14.0.0_rkr4"
+export KERNEL_BRANCH="iei-android-14.0.0_rkr5"
 export KERNEL_FLAVOR="rockchip"
 
 export EXTRA_PPAS="jjriek/rockchip jjriek/rockchip-multimedia"
