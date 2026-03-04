1d02c3d6ac5a8ac45434c5dea7ce51e993e49e70
lcwang <lcwang@ieiworld.com>
2026-03-04 17:45:39 +0800

    config: suites: pin iei-b675 noble kernel to a fixed commit
    Introduce KERNEL_COMMIT to lock kernel build input for reproducibility.
    
    Pinned commit: 72a134975c50a4af276fd1afc518a5c1b209d1a8
    
    Change-Id: I0d22a2357e879c9b84b0b1828b3632eb9542a04f

--- config/suites/iei-b675-noble.sh
+++ config/suites/iei-b675-noble.sh
@@ -5,6 +5,7 @@ export RELASE_VERSION="24.04"
 
 export KERNEL_REPO="https://github.com/QNAP-android-internal/kernel-rk.git"
 export KERNEL_BRANCH="iei-android-14.0.0_rkr5"
+export KERNEL_COMMIT="72a134975c50a4af276fd1afc518a5c1b209d1a8"
 export KERNEL_FLAVOR="rockchip"
 
 export EXTRA_PPAS="jjriek/rockchip jjriek/rockchip-multimedia"
